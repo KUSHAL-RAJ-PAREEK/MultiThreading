4b32ff6d45c6e5d5cdd1868489517c5262e5a81c
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2025-08-06 22:09:42 +0530

    read write lock

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,43 @@
 public class Main extends Thread{
 
     public static void main(String[] args) throws InterruptedException {
-      FairnessLockExample example = new FairnessLockExample();
-      Runnable task = new Runnable() {
-          @Override
-          public void run() {
-          example.accessResource();
-          }
-      };
 
-      Thread t1 = new Thread(task,"Thread 1");
-      Thread t2 = new Thread(task,"Thread 2");
-        Thread t3 = new Thread(task,"Thread 3");
-        Thread t4 = new Thread(task,"Thread 4");
+        ReadWriteCounter counter = new ReadWriteCounter();
 
-      t1.start();
-      t2.start();
-      t3.start();
-      t4.start();
+        Runnable readTask = new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < 10; i++){
+                    System.out.println(Thread.currentThread().getName()
+                    +" read: "+ counter.getCount());
+                }
+            }
+        };
+
+        Runnable writeTask = new Runnable() {
+            @Override
+            public void run() {
+                for(int i = 0; i < 10; i++){
+                    counter.increment();
+                    System.out.println(Thread.currentThread().getName()
+                            +" increment.");
+                }
+            }
+        };
+
+        Thread writerThread = new Thread(writeTask);
+        Thread readerThread1 = new Thread(readTask);
+        Thread readerThread2 = new Thread(readTask);
+
+        writerThread.start();
+        readerThread1.start();
+        readerThread2.start();
+
+        writerThread.join();
+        readerThread1.join();
+        readerThread2.join();
+
+        System.out.println("Final count: "+ counter.getCount());
+
     }
 }
