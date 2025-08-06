f21469b5857d0b431a74486319b6b1a460937bb1
KUSHAL RAJ PAREEK <115624967+KUSHAL-RAJ-PAREEK@users.noreply.github.com>
2025-08-06 19:24:09 +0530

    commit

---
+++ src/FairnessLockExample.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FairnessLockExample {
+
+    private final Lock lock = new ReentrantLock(true);
+
+
+    public void accessResource(){
+        lock.lock();
+
+        try{
+            System.out.println(
+                    Thread.currentThread().getName()
+                    +" acquired the lock."
+            );
+            Thread.sleep(3000);
+        }catch (Exception e){
+            Thread.currentThread().interrupt();
+        }finally {
+            System.out.println(Thread.currentThread().getName()
+            +" realeased the lock.");
+            lock.unlock();
+        }
+    }
+}
